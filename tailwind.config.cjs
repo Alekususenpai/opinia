112fb80a218ec0a2a73e09ae1ff98bc1b76e59e8
Alekususenpai <aa_lara@live.com>
2023-10-01 23:43:58 -0500

    btn class added to config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,5 +14,36 @@ module.exports = {
       // },
     },
   },
-  plugins: [],
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#050816",
+        },
+        // backgroundImage: {
+        //   "img-spinner": "url('/src/assets/bg.jpg')",
+        // },
+      },
+    },
+    extend: {
+      'btn': {
+        background: 'none',
+        cursor: 'pointer',
+        outline: 'none',
+        border: 'none',
+        textAlign: 'center',
+        transition: 'background-color 0.2s ease-in-out',
+        padding: '12px 24px',
+        borderRadius: '10px',
+        minWidth: '160px',
+        fontSize: '14px',
+      },
+    },
+  },
+    plugins: [],
 }
